9131979408cdc8aaaa9d05c1a551eed55e1c9f5c
Scott Luse <scottwluse@gmail.com>
2018-08-04 13:22:12 -0700

    Lesson07, login commits

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     path('', include('myblog.urls')),
     path('admin/', admin.site.urls),
+    path('login/', login, {'template_name': 'login.html'}, name="login"),
+    path('logout/', logout, {'next_page': '/'}, name="logout"),
 ]
