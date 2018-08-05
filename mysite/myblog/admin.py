2ce1c63993537b6d12c38d38b43085c90f8ed295
Scott Luse <scottwluse@gmail.com>
2018-08-04 17:24:13 -0700

    Assignment07, changed so admin can only add categories to posts

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,7 +1,25 @@
+#-------------------------------------------------#
+# Title: admin.py
+# Dev:   Scott Luse
+# Date:  Aug 04, 2018
+# Change so that you can only add categories to posts
+#-------------------------------------------------#
+
 from django.contrib import admin
 
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+admin.site.register(Category, CategoryAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date')
+
+admin.site.register(Post, PostAdmin)
+
--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,5 +1,23 @@
+#-------------------------------------------------#
+# Title: admin.py
+# Dev:   Scott Luse
+# Date:  Aug 04, 2018
+# Change so that you can only add categories to posts
+#-------------------------------------------------#
+
 from django.db import models
 from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    # posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -8,18 +26,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    categories = models.ManyToManyField(Category, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
-
-    def __str__(self):
-        return self.name
-
-
-    class Meta:
-        verbose_name_plural = 'Categories'
